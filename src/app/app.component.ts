3d1af938048cd172bf8f4c76402ef85fba0af260
ftoth <toth_ferenc@hotmail.com>
2024-02-07 22:41:48 +0100

    adding ace-builds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as ace from "ace-builds";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-rxjs-playground';
+  @ViewChild("editor") private editor!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    ace.config.set("fontSize", "14px");
+    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
+
+    const aceEditor = ace.edit(this.editor.nativeElement);
+    aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
+
+    aceEditor.setTheme('ace/theme/clouds');
+    aceEditor.session.setMode('ace/mode/typescript');
+
+    aceEditor.on("change", () => {
+      console.log(aceEditor.getValue());
+    });
+
+  }
 }
